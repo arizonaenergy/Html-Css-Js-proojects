980e1108a3802e3e115bbdfe4341e88221786de0
Dara <116750626+arizonaenergy@users.noreply.github.com>
2023-02-10 06:42:07 +0200

    Update script.js

--- Progress-Steps/script.js
+++ Progress-Steps/script.js
@@ -30,8 +30,8 @@ function update() {
     }
   });
   const actives = document.querySelectorAll(".active");
-  progress.style.width = ((actives.length - 1) / circles.length1) * 100 + "%";
-  //`${((actives.length - 1) / circles.length1) * 100}%`; //
+  progress.style.width = ((actives.length - 1) /( circles.length-1)) * 100 + "%";
+  
   if (currentActive === 1) {
     prev.disabled = true;
   } else if (currentActive === circles.lenght) {
